0a2ad16ca10ccb1f729149230d2f58459fd701e1
Andreas <andreas.just.petersen@gmail.com>
2025-03-18 13:09:42 +0100

    fuck

--- runexperiments.sh
+++ runexperiments.sh
@@ -1,11 +1,11 @@
 cd spot-stuff
-python scripts/rsl_rl/train.py --task=Msc-v0 --run_name="main"
+python scripts/rsl_rl/train.py --task=Msc-v0 --run_name="main" --headless
 git checkout catch_box_move
-python scripts/rsl_rl/train.py --task=Msc-v0 --run_name="catch_box_move"
+python scripts/rsl_rl/train.py --task=Msc-v0 --run_name="catch_box_move" --headless
 git checkout arm_move
-python scripts/rsl_rl/train.py --task=Msc-v0 --run_name="arm_move"
+python scripts/rsl_rl/train.py --task=Msc-v0 --run_name="arm_move" --headless
 git checkout tanh
-python scripts/rsl_rl/train.py --task=Msc-v0 --run_name="tanh"
+python scripts/rsl_rl/train.py --task=Msc-v0 --run_name="tanh" --headless
 git checkout good-boy-no-move
-python scripts/rsl_rl/train.py --task=Msc-v0 --run_name="goodboy"
+python scripts/rsl_rl/train.py --task=Msc-v0 --run_name="goodboy" --headless
 git checkout main
