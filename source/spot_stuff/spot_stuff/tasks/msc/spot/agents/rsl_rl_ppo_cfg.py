782f9f0a5f33b4a4aa04f17d4840826db4eb2bf7
Andreas <andreas.just.petersen@gmail.com>
2025-06-11 11:35:57 +0200

    Model 7.0

--- source/spot_stuff/spot_stuff/tasks/msc/spot/agents/rsl_rl_ppo_cfg.py
+++ source/spot_stuff/spot_stuff/tasks/msc/spot/agents/rsl_rl_ppo_cfg.py
@@ -17,19 +17,19 @@ class SpotFlatPPORunnerCfg(RslRlOnPolicyRunnerCfg):
     empirical_normalization = False
     store_code_state = False
     policy = RslRlPpoActorCriticCfg(
-        init_noise_std=1.5,
-        actor_hidden_dims=[512, 256, 128],
-        critic_hidden_dims=[512, 256, 128],
+        init_noise_std=0.5,
+        actor_hidden_dims=[256, 128, 64],
+        critic_hidden_dims=[256, 128, 64],
         activation="elu",
     )
     algorithm = RslRlPpoAlgorithmCfg(
         value_loss_coef=0.5,
         use_clipped_value_loss=True,
         clip_param=0.2,
-        entropy_coef=0.01,
+        entropy_coef=0.08,
         num_learning_epochs=5,
         num_mini_batches=4,
-        learning_rate=1.0e-3,
+        learning_rate=3.0e-4,
         schedule="adaptive",
         gamma=0.99,
         lam=0.95,
